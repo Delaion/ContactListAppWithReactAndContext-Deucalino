d2b646a453e56c27e42fa5189bead00aba79c8b1
Delaion <muvumafernando@gmail.com>
2023-08-03 22:30:29 +0000

    contact and add contact view files created, context coded

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -1,48 +1,52 @@
-import React, { useState, useEffect } from "react";
-import getState from "./flux.js";
+import React, {createContext, useState} from "react";
 
-// Don't change, here is where we initialize our context, by default it's just going to be null.
-export const Context = React.createContext(null);
+export const contactContext= createContext ();
 
-// This function injects the global store to any view/component where you want to use it, we will inject the context to layout.js, you can see it here:
-// https://github.com/4GeeksAcademy/react-hello-webapp/blob/master/src/js/layout.js#L35
-const injectContext = PassedComponent => {
-	const StoreWrapper = props => {
-		//this will be passed as the contenxt value
-		const [state, setState] = useState(
-			getState({
-				getStore: () => state.store,
-				getActions: () => state.actions,
-				setStore: updatedStore =>
-					setState({
-						store: Object.assign(state.store, updatedStore),
-						actions: { ...state.actions }
-					})
-			})
-		);
-
-		useEffect(() => {
-			/**
-			 * EDIT THIS!
-			 * This function is the equivalent to "window.onLoad", it only runs once on the entire application lifetime
-			 * you should do your ajax requests or fetch api requests here. Do not use setState() to save data in the
-			 * store, instead use actions, like this:
-			 *
-			 * state.actions.loadSomeData(); <---- calling this function from the flux.js actions
-			 *
-			 **/
-		}, []);
-
-		// The initial value for the context is not null anymore, but the current state of this component,
-		// the context will now have a getStore, getActions and setStore functions available, because they were declared
-		// on the state of this component
-		return (
-			<Context.Provider value={state}>
-				<PassedComponent {...props} />
-			</Context.Provider>
-		);
+const contactProvider=({children})=>{
+	const [contact, setContact]=useState({
+		fullname:"",
+		email:"",
+		phone:"",
+		adress:"",
+	})
+	const [inputContact, setInputContact]=useState([]);
+	const [editContact, setEditContact]=useState(null);
+	const addContact=(contact)=>{
+		if(editContact != null){
+			const newContactListEdited=inputContact.filter((contact,index)=> index != editContact)
+			setInputContact([... newContactListEdited,contact])
+			setEditContact(null) //Stop edditting
+			return
+		}
+		setInputContact ([...inputContact, contact]);
 	};
-	return StoreWrapper;
-};
+	const changeContext=(e)=>{
+		console.log(e.target.name);
+		//Actualize the contact:
+		setContact ({...contact, [e.target.name]: e.target.value});
+	};
 
-export default injectContext;
+    const deleteContact=(index)=>{
+		const newContactList=inputContact.filter((contact, i)=> index !==i)
+		setInputContact(newContactList)
+	};
+
+	const handleEditContact=(index)=>{
+		setEditContact(index);
+	};
+//Object context value:
+	const contextValue={
+		contact:contact,
+		contextFunction:changeContext,
+		addContact,
+		inputContact,
+		deleteContact,
+		handleEditContact,
+	};
+	return (
+		<contactContext.Provider value= {contextValue}>
+			{children}
+		</contactContext.Provider>
+	);
+};
+export default contactProvider;
